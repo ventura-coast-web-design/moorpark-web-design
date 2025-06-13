3b8a83efc297f2ac4b7cf16b1f6279d383c1834c
ackers93 <andrew.ackers@gmail.com>
2025-06-12 20:33:28 -0700

    added sass

---
+++ eleventy-config.js
@@ -0,0 +1,33 @@
+import path from "node:path";
+import * as sass from "sass";
+
+export default function (eleventyConfig) {
+	eleventyConfig.addExtension("scss", {
+		outputFileExtension: "css",
+
+		// opt-out of Eleventy Layouts
+		useLayouts: false,
+
+		compile: async function (inputContent, inputPath) {
+			let parsed = path.parse(inputPath);
+			// Don’t compile file names that start with an underscore
+			if(parsed.name.startsWith("_")) {
+				return;
+			}
+
+			let result = sass.compileString(inputContent, {
+				loadPaths: [
+					parsed.dir || ".",
+					this.config.dir.includes,
+				]
+			});
+
+			// Map dependencies for incremental builds
+			this.addDependencies(inputPath, result.loadedUrls);
+
+			return async (data) => {
+				return result.css;
+			};
+		},
+	});
+};
